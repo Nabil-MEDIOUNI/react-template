79be6482430489e4bd917d652d487cc016d3c533
unknown <ahmedjbeli54@yahoo.fr>
2021-02-27 22:57:16 +0100

    clean login

--- src/pages/authentification/signin/index.js
+++ src/pages/authentification/signin/index.js
@@ -45,42 +45,8 @@ export default function Login() {
             <div className="flex content-center items-center justify-center h-full">
               <div className="w-full lg:w-4/12 px-4">
                 <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-                  <div className="rounded-t mb-0 px-6 py-6">
-                    <div className="text-center mb-3">
-                      <h6 className="text-gray-600 text-sm font-bold">
-                        Sign in with
-                      </h6>
-                    </div>
-                    <div className="btn-wrapper text-center">
-                      <button
-                        className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        <img
-                          alt="..."
-                          className="w-5 mr-1"
-                          src={require('../../../assets/img/github.svg')}
-                        />
-                        Github
-                      </button>
-                      <button
-                        className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        <img
-                          alt="..."
-                          className="w-5 mr-1"
-                          src={require('../../../assets/img/google.svg')}
-                        />
-                        Google
-                      </button>
-                    </div>
-                    <hr className="mt-6 border-b-1 border-gray-400" />
-                  </div>
+                  <div className="rounded-t mb-0 px-6 py-6" />
                   <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <div className="text-gray-500 text-center mb-3 font-bold">
-                      <small>Or sign in with credentials</small>
-                    </div>
                     <form>
                       <div className="relative w-full mb-3">
                         <label
@@ -111,19 +77,6 @@ export default function Login() {
                           onChange={handleMany(person, addPerson, 'password')}
                         />
                       </div>
-                      <div>
-                        <label className="inline-flex items-center cursor-pointer">
-                          <input
-                            id="customCheckLogin"
-                            type="checkbox"
-                            className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                          />
-                          <span className="ml-2 text-sm font-semibold text-gray-700">
-                            Remember me
-                          </span>
-                        </label>
-                      </div>
-
                       <div className="text-center mt-6">
                         <button
                           onClick={clickSubmit}
@@ -145,11 +98,6 @@ export default function Login() {
                     >
                       <small>Forgot password?</small>
                     </a>
-                  </div>
-                  <div className="w-1/2 text-right">
-                    <Link to="/auth/register" className="text-gray-300">
-                      <small>Create new account</small>
-                    </Link>
                   </div>
                 </div>
               </div>
